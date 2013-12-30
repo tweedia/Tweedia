1b399be3493d8020d3488ecb7d427bba32fda22d
Takenobu Noda <takenobu.noda@gmail.com>
2013-12-30 22:46:26 +0900

    Add header comment

--- src/Tweedia/dlg-about.h
+++ src/Tweedia/dlg-about.h
@@ -1,3 +1,28 @@
+/* dlg-about.h */
+
+/* Copyright (C) 2013 Takenobu Noda
+
+   This file is part of Tweedia.
+
+   Tweedia is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Tweedia is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Tweedia. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* Birthdate:
+   Dec 30, 2013; Saitama, Japan
+   Initial author: Takenobu Noda
+ */
+
 #ifndef DLGABOUT_H
 #define DLGABOUT_H
 
--- src/Tweedia/dlg-create-resulttable.cpp
+++ src/Tweedia/dlg-create-resulttable.cpp
@@ -1,3 +1,28 @@
+/* dlg-create-resulttable.cpp */
+
+/* Copyright (C) 2013 Takenobu Noda
+
+   This file is part of Tweedia.
+
+   Tweedia is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Tweedia is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Tweedia. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* Birthdate:
+   Dec 20, 2013; Saitama, Japan
+   Initial author: Takenobu Noda
+ */
+
 #include "dlg-create-resulttable.h"
 #include "ui_dlg-create-resulttable.h"
 
